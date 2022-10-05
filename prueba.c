5732f770976fac06c2610986683657bd8a27ffa6
José Aguirre <josealfredo02aguirre@gmial.com>
2022-10-05 00:14:40 -0400

    05/10/22 agregando el hola mundo de C

---
+++ prueba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Que arrechera con C\n");
+	printf("El video que mando el profe no sirve\n");
+	printf("Creo que me equivoque de carrera :( pta vida\n");
+	printf("Adios mundo cruel\n");
+	getchar();
+	return 0;
+
+}
